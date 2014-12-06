453b4806a685e0e1f96c5d592852a7469d6750bd
vsymguysung <guysung@vsym.com>
2014-12-06 12:39:52 -0500

    Create summer-note component files for both the addon and the app.

---
+++ addon/components/summer-note.js
@@ -0,0 +1,11 @@
+import Ember from "ember";
+
+var get = Ember.get;
+
+
+var SummerNoteComponent = Ember.Component.extend({
+
+
+});
+
+export default SummerNoteComponent;
---
+++ app/components/summer-note.js
@@ -0,0 +1,7 @@
+/*
+	This is just a proxy file requiring the component from the /addon folder and
+	making it available to the dummy application!
+ */
+import SummerNoteComponent from 'ember-cli-bootstrap3-wysiwyg/components/bs-wysihtml5';
+ 
+export default SummerNoteComponent;
