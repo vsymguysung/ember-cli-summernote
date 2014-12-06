4c3d9cced87f838ce0c0a30a7882d9c4218531d5
vsymguysung <guysung@vsym.com>
2014-12-06 12:34:42 -0500

    Implement addon index.js

--- index.js
+++ index.js
@@ -2,5 +2,22 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-summernote'
+
+  name: 'ember-cli-summernote',
+
+
+  included: function(app) {
+    this._super.included(app);
+
+    // <!-- include libraries(jQuery, bootstrap, fontawesome) -->
+    app.import(app.bowerDirectory + '/jquery/dis/jquery.min.js');
+    app.import(app.bowerDirectory + '/bootstrap/dist/css/bootstrap.min.css');
+    app.import(app.bowerDirectory + '/bootstrap/dist/js/bootstrap.min.js');
+    app.import(app.bowerDirectory + '/fontawesome/css/font-awesome.min.js');
+
+    // <!-- include summernote css/js-->
+    app.import(app.bowerDirectory + '/summernote/dist/summernote.css');
+    app.import(app.bowerDirectory + '/summernote/dist/summernote.js');
+
+  }
 };
