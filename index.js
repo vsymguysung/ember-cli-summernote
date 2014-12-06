bfc3fe3119cb0c8c27d05a089bf03c72954dfb21
vsymguysung <guysung@vsym.com>
2014-12-06 16:22:16 -0500

    Implement the included hook in index.js

--- index.js
+++ index.js
@@ -9,15 +9,24 @@ module.exports = {
   included: function(app) {
     this._super.included(app);
 
-    // <!-- include libraries(jQuery, bootstrap, fontawesome) -->
-    app.import(app.bowerDirectory + '/jquery/dist/jquery.min.js');
+    // jQuery
+    app.import(app.bowerDirectory + '/jquery/dist/jquery.js'); // jquery.min.js is not working...
+
+    // Bootstrap
     app.import(app.bowerDirectory + '/bootstrap/dist/css/bootstrap.min.css');
     app.import(app.bowerDirectory + '/bootstrap/dist/js/bootstrap.min.js');
-    app.import(app.bowerDirectory + '/fontawesome/css/font-awesome.min.css');
 
-    // <!-- include summernote css/js-->
+    // font awesome
+    app.import(app.bowerDirectory + '/fontawesome/css/font-awesome.min.css');
+    app.import(app.bowerDirectory + '/fontawesome/fonts/fontawesome-webfont.eot', { destDir: 'fonts' });
+    app.import(app.bowerDirectory + '/fontawesome/fonts/fontawesome-webfont.svg', { destDir: 'fonts' });
+    app.import(app.bowerDirectory + '/fontawesome/fonts/fontawesome-webfont.ttf', { destDir: 'fonts' });
+    app.import(app.bowerDirectory + '/fontawesome/fonts/fontawesome-webfont.woff', { destDir: 'fonts' });
+    app.import(app.bowerDirectory + '/fontawesome/fonts/FontAwesome.otf', { destDir: 'fonts' });
+
+    // Summernote
     app.import(app.bowerDirectory + '/summernote/dist/summernote.css');
-    app.import(app.bowerDirectory + '/summernote/dist/summernote.js');
+    app.import(app.bowerDirectory + '/summernote/dist/summernote.min.js');
 
   }
 };
