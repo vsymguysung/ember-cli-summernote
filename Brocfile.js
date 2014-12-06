961bf777cc786947427741ee1188a86cffe670a5
vsymguysung <guysung@vsym.com>
2014-12-06 18:05:54 -0500

    Fix dependency issue.

--- Brocfile.js
+++ Brocfile.js
@@ -18,4 +18,20 @@ var app = new EmberAddon();
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
 
+
+// jQuery
+app.import(app.bowerDirectory + '/jquery/dist/jquery.js'); // jquery.min.js is not working...
+
+// Bootstrap
+app.import(app.bowerDirectory + '/bootstrap/dist/css/bootstrap.min.css');
+app.import(app.bowerDirectory + '/bootstrap/dist/js/bootstrap.min.js');
+
+// font awesome
+app.import(app.bowerDirectory + '/fontawesome/css/font-awesome.min.css');
+app.import(app.bowerDirectory + '/fontawesome/fonts/fontawesome-webfont.eot', { destDir: 'fonts' });
+app.import(app.bowerDirectory + '/fontawesome/fonts/fontawesome-webfont.svg', { destDir: 'fonts' });
+app.import(app.bowerDirectory + '/fontawesome/fonts/fontawesome-webfont.ttf', { destDir: 'fonts' });
+app.import(app.bowerDirectory + '/fontawesome/fonts/fontawesome-webfont.woff', { destDir: 'fonts' });
+app.import(app.bowerDirectory + '/fontawesome/fonts/FontAwesome.otf', { destDir: 'fonts' });
+
 module.exports = app.toTree();
--- index.js
+++ index.js
@@ -9,21 +9,6 @@ module.exports = {
   included: function(app) {
     this._super.included(app);
 
-    // jQuery
-    app.import(app.bowerDirectory + '/jquery/dist/jquery.js'); // jquery.min.js is not working...
-
-    // Bootstrap
-    app.import(app.bowerDirectory + '/bootstrap/dist/css/bootstrap.min.css');
-    app.import(app.bowerDirectory + '/bootstrap/dist/js/bootstrap.min.js');
-
-    // font awesome
-    app.import(app.bowerDirectory + '/fontawesome/css/font-awesome.min.css');
-    app.import(app.bowerDirectory + '/fontawesome/fonts/fontawesome-webfont.eot', { destDir: 'fonts' });
-    app.import(app.bowerDirectory + '/fontawesome/fonts/fontawesome-webfont.svg', { destDir: 'fonts' });
-    app.import(app.bowerDirectory + '/fontawesome/fonts/fontawesome-webfont.ttf', { destDir: 'fonts' });
-    app.import(app.bowerDirectory + '/fontawesome/fonts/fontawesome-webfont.woff', { destDir: 'fonts' });
-    app.import(app.bowerDirectory + '/fontawesome/fonts/FontAwesome.otf', { destDir: 'fonts' });
-
     // Summernote
     app.import(app.bowerDirectory + '/summernote/dist/summernote.css');
     app.import(app.bowerDirectory + '/summernote/dist/summernote.min.js');
